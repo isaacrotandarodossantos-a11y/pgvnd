830943097a5fd9350183904cf16e1042c21744ce
Isaac Rotandaro dos Santos <isaacrotandarodossantos@gmail.com>
2026-07-14 13:20:06 -0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 from apimercadopago import gerar_link_pagamento
@@ -25,4 +26,6 @@ def compra_errada():
     return "Erro no pagamento ou cancelado."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Ajuste para rodar corretamente no Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
