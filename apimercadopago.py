bec6de3045873256a021fc1a3f40589d05fa847c
Isaac Rotandaro dos Santos <isaacrotandarodossantos@gmail.com>
2026-07-15 16:16:32 -0300

    apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -13,7 +13,7 @@ def gerar_link_pagamento(nome, email, cpf):
                 "title": "Inscrição São Jorge Para Todos",
                 "quantity": 1,
                 "currency_id": "BRL",
-                "unit_price": 0.01
+                "unit_price": 1.00
             }
         ],
         "payer": {
