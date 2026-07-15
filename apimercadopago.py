7e19454ee7b79b55fca897b46b16b8df909c3cb1
Isaac Rotandaro dos Santos <isaacrotandarodossantos@gmail.com>
2026-07-15 19:59:55 -0300

    apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -9,7 +9,7 @@ def gerar_link_pagamento(nome):  # Mantemos o nome da função para não quebrar
     request_options.idempotency_key = str(uuid.uuid4())
 
     payment_data = {
-        "transaction_amount": 50.00,
+        "transaction_amount": 0.01,
         "description": "Inscrição São Jorge Para Todos",
         "payment_method_id": "pix",  # Força o método Pix
         "payer": {
