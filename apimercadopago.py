cec00ba3ecdcb8407275aa0cfae7febdbada41e1
Isaac Rotandaro dos Santos <isaacrotandarodossantos@gmail.com>
2026-07-15 15:24:49 -0300

    apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -2,7 +2,7 @@ import mercadopago
 import os
 
 def gerar_link_pagamento(nome, email, cpf):
-    token = os.getenv("MP_TOKEN")
+    token = os.getenv("APP_USR-8677986015174769-071410-f913279c1c5f01176f80d34cac8c035b-722783171")
     sdk = mercadopago.SDK(token)
 
     url_sucesso = "https://bucolic-fox-ea9bba.netlify.app/"
@@ -13,7 +13,7 @@ def gerar_link_pagamento(nome, email, cpf):
                 "title": "Inscrição São Jorge Para Todos",
                 "quantity": 1,
                 "currency_id": "BRL",
-                "unit_price": 79.90
+                "unit_price": 60.00
             }
         ],
         "external_reference": cpf, # <--- ESSENCIAL: Vincula o pagamento ao CPF
