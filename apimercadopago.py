b8d9ab88781e7fca32a06fcb50416e851da54445
Isaac Rotandaro dos Santos <isaacrotandarodossantos@gmail.com>
2026-07-16 08:10:23 -0300

    apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -18,7 +18,7 @@ def gerar_link_pagamento():
     # FLUXO 1: SE O USUÁRIO ESCOLHER PIX
     if metodo == "pix":
         payment_data = {
-            "transaction_amount": 0.0q,
+            "transaction_amount": 0.01,
             "description": "Inscrição São Jorge Para Todos",
             "payment_method_id": "pix",
             "payer": {
